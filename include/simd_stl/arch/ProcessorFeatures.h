9a9e8a3570ec25773cef24cd633aaf7a84c18b0f
Midoritotoro <danya20090504@gmail.com>
2025-09-01 23:39:54 +0300

    Processor features

--- include/simd_stl/arch/ProcessorFeatures.h
+++ include/simd_stl/arch/ProcessorFeatures.h
@@ -37,89 +37,122 @@ private:
     public:
         ProcessorFeaturesInternal() noexcept;
 
-        std::bitset<32> _leaf1EcxBitset;
-        std::bitset<32> _leaf1EdxBitset;
-        std::bitset<32> _leaf7EbxBitset;
+        bool _sse      : 1 = false;
+        bool _sse2     : 1 = false;
+        bool _sse3     : 1 = false;
+        bool _sse41    : 1 = false;
+        bool _sse42    : 1 = false;
+        bool _ssse3    : 1 = false;
+        
+        bool _avx      : 1 = false;
+        bool _avx2     : 1 = false;
+
+        bool _avx512f  : 1 = false;
+        bool _avx512bw : 1 = false;
+        bool _avx512pf : 1 = false;
+        bool _avx512er : 1 = false;
+        bool _avx512cd : 2 = false;
+        bool _avx512vl : 2 = false;
     };
 
     static inline ProcessorFeaturesInternal _processorFeaturesInternal;
 };
 
 ProcessorFeatures::ProcessorFeaturesInternal::ProcessorFeaturesInternal() noexcept {
-    std::array<uint32, 4>               registers;
-    std::vector<std::array<uint32, 4>>  data;
+    std::array<uint32, 4> registers;
 
     cpuid(registers.data(), 0);
-    const auto leafNumber = registers[0];
-
-    for (int i = 0; i <= leafNumber; ++i) {
-        cpuidex(registers.data(), i, 0);
-        data.push_back(registers);
-    }
+    const auto leafCount = registers[0];
  
-    if (leafNumber >= 1) {
-        _leaf1EcxBitset = data[1][2]; // leaf 1
-        _leaf1EdxBitset = data[1][3]; // leaf 1
+    if (leafCount >= 1) {
+        memset(registers.data(), 0, registers.size() * sizeof(uint32));
+        cpuidex(registers.data(), 1, 0); // 0 - eax, 1 - ebx, 2 - ecx, 3 - edx
+
+        const auto leaf1Ecx = registers[2];
+        const auto leaf1Edx = registers[3];
+
+        _sse    = (leaf1Edx >> 25) & 1;
+        _sse2   = (leaf1Edx >> 26) & 1;
+        
+        _sse3   = (leaf1Ecx & 1);
+        _ssse3  = (leaf1Ecx >> 9) & 1;
+        _sse41  = (leaf1Ecx >> 19) & 1;
+        _sse42  = (leaf1Ecx >> 20) & 1;
+        
+        _avx    = (leaf1Ecx >> 28) & 1;
     }
 
-    if (leafNumber >= 7)
-        _leaf7EbxBitset = data[7][1]; // leaf 7
+    if (leafCount >= 7) {
+        memset(registers.data(), 0, registers.size() * sizeof(uint32));
+        cpuidex(registers.data(), 7, 0); // 0 - eax, 1 - ebx, 2 - ecx, 3 - edx
+
+        const auto leaf7Ebx = registers[1];
+        
+        _avx2       = (leaf7Ebx >> 5) & 1;
+
+        _avx512f    = (leaf7Ebx >> 16) & 1;
+        _avx512bw   = (leaf7Ebx >> 30) & 1;
+        _avx512pf   = (leaf7Ebx >> 26) & 1;
+        _avx512er   = (leaf7Ebx >> 27) & 1;
+        _avx512cd   = (leaf7Ebx >> 28) & 1;
+        _avx512vl   = (leaf7Ebx >> 31) & 1;
+    }
 }
 
 bool ProcessorFeatures::SSE() noexcept {
-    return _processorFeaturesInternal._leaf1EdxBitset[25];
+    return _processorFeaturesInternal._sse;
 }
 
 bool ProcessorFeatures::SSE2() noexcept {
-    return _processorFeaturesInternal._leaf1EdxBitset[26];
+    return _processorFeaturesInternal._sse2;
 }
 
 bool ProcessorFeatures::SSE3() noexcept {
-    return _processorFeaturesInternal._leaf1EcxBitset[0];
+    return _processorFeaturesInternal._sse3;
 }
 
 bool ProcessorFeatures::SSSE3() noexcept {
-    return _processorFeaturesInternal._leaf1EcxBitset[9];
+    return _processorFeaturesInternal._ssse3;
 }
 
 bool ProcessorFeatures::SSE41() noexcept {
-    return _processorFeaturesInternal._leaf1EcxBitset[19];
+    return _processorFeaturesInternal._sse41;
 }
 
 bool ProcessorFeatures::SSE42() noexcept {
-    return _processorFeaturesInternal._leaf1EcxBitset[20];
+    return _processorFeaturesInternal._sse42;
 }
 
 bool ProcessorFeatures::AVX() noexcept {
-    return _processorFeaturesInternal._leaf1EcxBitset[28];
+    return _processorFeaturesInternal._avx;
 }
 
 bool ProcessorFeatures::AVX2() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[5];
+    return _processorFeaturesInternal._avx2;
 }
 
 bool ProcessorFeatures::AVX512F() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[16];
+    return _processorFeaturesInternal._avx512f;
 }
 
 bool ProcessorFeatures::AVX512BW() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[30];
+    return _processorFeaturesInternal._avx512bw;
 }
 
 bool ProcessorFeatures::AVX512PF() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[26];
+    return _processorFeaturesInternal._avx512pf;
 }
 
 bool ProcessorFeatures::AVX512ER() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[27];
+    return _processorFeaturesInternal._avx512er;
 }
 
 bool ProcessorFeatures::AVX512CD() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[28];
+    return _processorFeaturesInternal._avx512cd;
 }
 
 bool ProcessorFeatures::AVX512VL() noexcept {
-    return _processorFeaturesInternal._leaf7EbxBitset[31];
+    return _processorFeaturesInternal._avx512vl;
 }
 
 __SIMD_STL_ARCH_NAMESPACE_END
